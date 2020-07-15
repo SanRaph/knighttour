5ff68e5c14d9cdba37b782ba093cfd2e902861d5
SanRaph <raphytex@gmail.com>
2020-07-15 10:51:44 +0100

    re-iterate and complete pending TODOs

--- src/KnightTourCohortFour.java
+++ src/KnightTourCohortFour.java
@@ -1,9 +1,24 @@
+/**************************************************
+ * Author: Raphael
+ * Date: Wednesday July 15
+ * Problem:
+ * 7.22 (Knight’s Tour) An interesting puzzler for chess buffs is the Knight’s
+ * Tour problem, originally proposed by the mathematician Euler. Can the knight
+ * piece move around an empty chessboard and touch each of the 64 squares once and
+ * only once? We study this intriguing problem in depth here. The knight makes only
+ * L-shaped moves (two spaces in one direction and one space in a perpendicular direction).
+ * Thus, as shown in Fig. 7.30, from a square near the middle of an empty chessboard, the knight
+ * (labeled K) can make eight different moves (numbered 0 through 7).
+ **************************************************/
+
+
 import java.text.DecimalFormat;
 
 public class KnightTourCohortFour {
     int[][] solution;
     int path = 0;
 
+    //TODO: Initialize solution to Zero
     public KnightTourCohortFour(int N) {
         solution = new int[N][N];
         for (int i = 0; i < N; i++) {
@@ -13,6 +28,7 @@ public class KnightTourCohortFour {
         }
     }
 
+    //TODO: Find possible available movement
     public void solve() {
         if (findPath(0, 0, 0, solution.length)) {
             print();
@@ -21,14 +37,15 @@ public class KnightTourCohortFour {
         }
     }
 
+    //TODO: Write the method to detect open moves
     public boolean findPath(int row, int column, int index, int N) {
         // check if current is not used already
         if (solution[row][column] != 0) {
             return false;
         }
-        // mark the current cell is as used
+        // mark the current cell if already used
         solution[row][column] = path++;
-        // if (index == 50) {
+        // if (index == 50)
         if (index == N * N - 1) {
             // if we are here means we have solved the problem
             return true;
@@ -75,7 +92,7 @@ public class KnightTourCohortFour {
                 && findPath(row + 2, column - 1, index + 1, N)) {
             return true;
         }
-        // if we are here means nothing has worked , backtrack
+        // if we are here means nothing has worked , backtrack by re-initializing solution to Zero and decrementing path to reverse
         solution[row][column] = 0;
         path--;
         return false;
